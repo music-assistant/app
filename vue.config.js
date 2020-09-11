4ee4000e4523c66bf3b39fb4b705628444a5496b
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-09-11 12:59:21 +0200

    initial version

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    },
+    cordovaPath: 'src-cordova'
+  },
+  devServer: {
+    https: false
+  },
+  pwa: {
+    name: 'Music Assistant',
+    themeColor: '#424242',
+    msTileColor: '#424242',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  },
+  transpileDependencies: [
+    'vuetify'
+  ],
+  outputDir: '../../music-assistant.github.io/app',
+  publicPath: ''
+}
